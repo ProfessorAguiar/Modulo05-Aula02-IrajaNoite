b45f944565d0c14db5ca22da51796a3846c9a35c
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-09 19:29:15 -0300

    Atividade Aula 03

--- HelloExpress.js
+++ HelloExpress.js
@@ -1,8 +1,11 @@
 import express from 'express'
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
-  res.send('Hello Express!')
+app.get('/usuario', (req, res) => {
+    res.send('Rota Raiz')
+  })
+app.get('/contato', (req, res) => {
+  res.send('Rota Contato')
 })
 app.listen(port, () => {
   console.log(`Servidor Rodando em: http://localhost:${port}`)
