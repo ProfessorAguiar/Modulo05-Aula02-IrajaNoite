d71ba4a6f5b350e053ca173c776670e507865253
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-10 19:21:20 -0300

    Rotas Usuário e Tarefas

--- HelloExpress.js
+++ HelloExpress.js
@@ -2,10 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 app.get('/usuario', (req, res) => {
-    res.send('Rota Raiz')
+    res.send('Rota Usuario com metodo GET')
   })
-app.get('/contato', (req, res) => {
-  res.send('Rota Contato')
+app.get('/tarefa', (req, res) => {
+  res.send('Rota tarefa com metodo GET')
 })
 app.listen(port, () => {
   console.log(`Servidor Rodando em: http://localhost:${port}`)
