972212a6b4e9a151d4e55d0c57a437a0df866ab0
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-25 14:41:57 -0300

    alt bd e imgs

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from 'express'
 import UsuarioController from './controllers/usuarioController.js'
 import TarefaController from './controllers/tarefaController.js'
 import rootController from './controllers/rootController.js'
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.json())
 UsuarioController(app)
 TarefaController(app)
