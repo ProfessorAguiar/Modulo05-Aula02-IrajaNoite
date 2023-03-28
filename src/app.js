fefd7c3a437f96449a5ca0277e3cb18024bd7e51
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-28 15:25:19 -0300

    28-03

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import rootController from './controllers/rootController.js'
 import cors from 'cors'
 import session from 'cookie-session'
 const app = express()
-app.use(cors())
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*")
+  res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE")
+  res.header("Access-Control-Allow-Headers","X-PINGOTHER,Content-Type, Authorization")
+  app.use(cors())
+  next()
+})
 app.use(express.json())
 // const sess = {
 //   secret: 'keyboard cat',
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,13 @@
 import { users } from "../infra/bd.js"
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
+import multer from 'multer'
+import path from "path";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 function UsuarioController(app) {
     app.get('/usuario', exibir)
     function exibir(req, res) {
@@ -14,23 +21,25 @@ function UsuarioController(app) {
             db.close()
         })()
     }
-    // app.get('/usuario', exibir)
-    // function exibir(req, res) {
-    //     (async () => {
-    //         const db = await open({
-    //             filename: './src/infra/bdTarefas.db',
-    //             driver: sqlite3.Database
-    //         })
-    //         const sql = 'SELECT * FROM Usuario'
-    //         db.each(sql,(err, row) => {
-    //             if (err) {
-    //               throw err;
-    //             }
-    //             res.send(`${row.nome} ${row.email} - ${row.senha}`);
-    //           });
-    //           db.close()
-    //     })()
-    // }
+
+    const storage = multer.diskStorage({
+        // dest: './src/imgs/users/',
+        destination: function (req, file, cb) {
+            cb(null, './src/imgs/users/')
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname) //Appending .jpg
+        }
+
+    })
+    const upload = multer({ storage: storage });
+    app.post('/usuario/upload', upload.single('image'), (req, res, next) => {
+        console.log(req)
+        res.send(req.file)
+
+    })
+
+
     app.get('/usuario/email/:email', buscarEmail)
     function buscarEmail(req, res) {
         (async () => {
@@ -47,6 +56,8 @@ function UsuarioController(app) {
             db.close()
         })()
     }
+
+
     app.get('/usuario/nome/:nome', buscarNome)
     function buscarNome(req, res) {
         (async () => {
@@ -63,6 +74,30 @@ function UsuarioController(app) {
             db.close()
         })()
     }
+
+
+    app.post('/usuario/login/:login', buscarLogin)
+    function buscarLogin(req, res) {
+        (async () => {
+            const db = await open({
+                filename: './src/infra/bdTarefas.db',
+                driver: sqlite3.Database
+            })
+            const result = await db.all(`SELECT * FROM Usuario where email like ?`, req.params.login)
+            if (result != '') {
+                if (result[0].senha == req.body.senha) {
+                    res.send(true)
+                } else {
+                    res.send(false)
+                }
+            } else {
+                res.send(`email ${req.params.login} não encontrado.`)
+            }
+            db.close()
+        })()
+    }
+
+
     app.post('/usuario', inserir)
     function inserir(req, res) {
         (async () => {
@@ -70,7 +105,7 @@ function UsuarioController(app) {
                 filename: './src/infra/bdTarefas.db',
                 driver: sqlite3.Database
             })
-            await db.run(`INSERT INTO Usuario(nome,email,senha) VALUES(?,?,?)`, req.body.nome, req.body.email, req.body.senha)
+            await db.run(`INSERT INTO Usuario(nome,email,senha,CEP,endereco,complemento,CPF,numero,foto,cidade,estado) VALUES(?,?,?,?,?,?,?,?,?,?,?)`, req.body.nome, req.body.email, req.body.senha, req.body.CEP, req.body.endereco, req.body.complemento, req.body.CPF, req.body.numero, req.body.foto, req.body.cidade, req.body.estado)
             res.send(`Usuário: ${req.body.nome} inserido com sucesso.`)
             db.close()
         })()
@@ -124,7 +159,7 @@ function UsuarioController(app) {
                 res.send(`Usuário: ${req.params.email} não encontrado`)
             }
             db.close()
-        })() 
+        })()
     }
 }
 export default UsuarioController
